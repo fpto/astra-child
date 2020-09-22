d5c3376e21008d5e97db34df0db7971d29f4bf81
fpto <fabricio.puerto@outlook.com>
2020-09-22 17:21:53 -0600

    Added taxes rows

--- woocommerce/pdf/CustomPK/invoice.php
+++ woocommerce/pdf/CustomPK/invoice.php
@@ -405,6 +405,7 @@
 
 
 					// $i = 4;
+					$taxes_list = array();
 					foreach ($all_taxes as $key => $value) {
 						$all_tx_s = array();
 						$v = preg_replace( '/[^.\d]/', '', $value['value'] );
@@ -419,7 +420,8 @@
 
 							$array_new_one[] = $all_tx_s;
 
-							array_splice( $pk_final_total, $i, 0, $all_tx_s );
+							// array_splice( $pk_final_total, $i, 0, $all_tx_s );
+							$taxes_list += $all_tx_s;
 							$i++;
 						}
 					}
@@ -466,6 +468,14 @@
 						<td style="text-align: center;" class="price_new"><span class="totals-price"><?php echo $base['value']?>;
 					</tr>
 				<?php endforeach; ?>
+				<?php foreach($taxes_list as $key => $tax): ?>
+				 <tr>
+					 <td class="no-borders"></td>
+					 <th class="description_new"><?php echo $base['label']; ?></th>
+					 <td style="text-align: center;" class="price_new"><span class="totals-price"><?php echo $tax['value']?>;
+				 </tr>
+			 <?php endforeach; ?>
+
 				 		<?php unset($pk_final_total['cart_subtotal']) //remove subtotal ?>
 						<?php foreach( $pk_final_total as $key => $total ) : ?>
 							<tr class="<?php echo $key.'new'; ?>">
