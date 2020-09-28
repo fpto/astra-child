06804078c185cf8d50c48d5b5b522a7c41a8309b
fpto <fabricio.puerto@outlook.com>
2020-09-28 15:30:32 -0600

    Went back to try to fix ship no tax

--- woocommerce/pdf/CustomPK/invoice.php
+++ woocommerce/pdf/CustomPK/invoice.php
@@ -404,16 +404,8 @@
 							'base_value_'.$value['percent'].'_percent' =>
 								array(
 								'label' => "<span style='text-transform: capitalize;'>"."IMPORTE GRAVADO AL ".$value['percent']."%</span>",
-<<<<<<< HEAD
-<<<<<<< HEAD
-								'value' => "L".number_format((float)$value['value'], 2, '.', '')
-=======
 								'value' => $ship_no_tax
->>>>>>> parent of b525b21... Format ship no tax.
-=======
-								'value' => $ship_no_tax
->>>>>>> parent of b525b21... Format ship no tax.
-							));
+								));
 							// array_splice( $pk_final_total, $i, 0, $new_tax_base_a );
 							$tax_bases += $new_tax_base_a;
 						}
